339439122db6a519f063334be59e3235f8d0e3b6
Sam Wood <sam@sam.uk.net>
2015-02-07 23:36:44 +0000

    added playlist functionality (press the fucking + key)

--- raspi/main.py
+++ raspi/main.py
@@ -12,14 +12,25 @@ bindings = dbase.getBindings()
 names = dbase.getNames()
 dbase.close()
 
+songs = ["http://192.168.5.122/fridge/webapp/song.mp3"]
+
+
+def playSound(url):
+     subprocess.call(["omxplayer", url]) 
 
 while 1:
     char = getch.getch()
+
+    if(char == 43):
+        songs.append(songs[0])
+        playSound(songs[0])
+        songs.pop(0)
+    
     try:
         parser = inParser.InputParser(char, bindings)
         
         itemId = parser.getValue()
-
+        
         dbase = db.DB()
         dbase.connect()
 
@@ -32,4 +43,4 @@ while 1:
         dbase.close()
         subprocess.call('echo '+text+'|festival --tts', shell=True)
     except:
-        print "Press correct buttonn you shithead"
+        print "Press correct button you shithead"
