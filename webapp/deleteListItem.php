7fe8a254e48578e30b1f158c6aa62a0570920cb7
Ultron XIV <pmb14@ic.ac.uk>
2015-02-07 15:50:15 +0000

    prepared statements in delete php

--- webapp/deleteListItem.php
+++ webapp/deleteListItem.php
@@ -1,11 +1,16 @@
 <?php
 include("includes/meta.php");
+$stmt = $dbh->prepare("DELETE FROM lists WHERE pID=? AND itemID=?");
+$stmt->bindParam(1, $pID);
+$stmt->bindParam(2, $itemID);
 
 $pID = $_GET["pID"];
 $itemID = $_GET["itemID"];
 
-$deleteRow = "DELETE FROM lists WHERE pID=$pID AND itemID=$itemID";
+#$deleteRow = "DELETE FROM lists WHERE pID=$pID AND itemID=$itemID";
 
-mysql_query($deleteRow) or die(mysql_error());
+#mysql_query($deleteRow) or die(mysql_error());
+
+$stmt->execute();
 
 ?>
