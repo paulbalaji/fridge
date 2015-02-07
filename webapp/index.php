22ba1711fade669b49f5c6df12b2d87f9ce32aa1
Sam Wood <sam@sam.uk.net>
2015-02-07 14:30:35 +0000

    Basic JQuery page.  Folder infrastructure added.

--- webapp/index.php
+++ webapp/index.php
@@ -1,1 +1,64 @@
-Hello World
+<?php
+include("includes/meta.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    
+
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    
+    <link rel = "stylesheet" type="text/css" href="lib/universal.css" />    
+    
+
+    <script type="text/javascript">
+      $(function() {
+         $("#list ul li").on("swiperight",function(){
+  $(this).hide();
+    });
+ 
+          
+      });
+    </script>
+    
+</head>
+<body>
+
+<div data-role="page">
+  <div data-role="header">
+      
+      <h1> Fridge App</h1>
+  <div data-role="navbar">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="settings.php">Settings</a></li>
+      <li><a href="changepID.php">Enter PI ID</a></li>
+    </ul>
+  </div>
+</div>
+
+  <div data-role="main" class="ui-content">
+    <p>The List</p>
+      <div id="list">
+          <?php
+                #include("getList.php");
+            ?>
+          <ul>
+          
+              
+              
+          </ul>
+      </div>
+  </div>
+
+  <div data-role="footer">
+    <h1>Settings</h1>
+  </div>
+</div> 
+
+</body>
+</html>
