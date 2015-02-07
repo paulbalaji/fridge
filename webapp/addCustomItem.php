b611fee012b0f6fa1dfb07e5d051f45339c36a60
Sam Wood <sam@sam.uk.net>
2015-02-07 17:53:42 +0000

    ...

---
+++ webapp/addCustomItem.php
@@ -0,0 +1,22 @@
+<?php
+include("includes/meta.php");
+
+$pID = $_POST['pID'];
+$itemID = -1;
+$customItemName = $_POST['customItemName'];
+
+
+$query = "SELECT * FROM lists WHERE customItemName = '$customItemName'";
+$result = mysql_query($query) or die(mysql_error());
+echo (sizeof(mysql_fetch_array($result)))  ;
+$present = true;
+if(!$present) {
+
+$query = "INSERT INTO lists(pID, itemID, customItemName) VALUES ( $pID, $itemID, '$customItemName')";
+echo $query;
+mysql_query($query) or die(mysql_error());
+}
+
+#Header("Location:index.php?msg=hehe");
+
+?>
---
+++ webapp/settings.php
@@ -0,0 +1,53 @@
+<?php include( "includes/meta.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+    <script src="lib/home.js"></script>
+    <link rel="stylesheet" type="text/css" href="lib/universal.css" />
+
+ 
+
+</head>
+
+<body>
+    <?php #echo $pID; ?>
+    <div data-role="page">
+
+        <div data-role="header">
+
+            <h1>Settings</h1>
+            <div data-role="navbar">
+                <ul>
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li><a href="settings.php">Settings</a>
+                    </li>
+                    <li><a href="changepID.php">Enter PI ID</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+	
+        
+        <div data-role="main" class="ui-content">
+            <p>Settings</p>
+            <div id="list">
+                
+            </div>
+        </div>
+
+        <div data-role="footer">
+            <h1>End of Page</h1>
+        </div>
+    </div>
+
+</body>
+
+</html>
