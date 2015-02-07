1f06c5c9342d081ddae144cbc6d0ca386dccb1b6
Ultron XIV <pmb14@ic.ac.uk>
2015-02-07 14:34:22 +0000

    added some php shit

---
+++ webapp/getList.php
@@ -0,0 +1,18 @@
+<?php
+#connect to MySQL
+include("includes/meta.php");
+
+$query = "SELECT itemID FROM lists WHERE pID=".$pID;
+
+$result = mysql_query($query) or die(mysql_error());
+
+while($row = mysql_fetch_array($result)){
+    $itemID = $row['itemID'];
+    $subQuery = "SELECT itemName from items WHERE itemID=".$itemID;
+    
+    $subResult = mysql_query($subQuery) or die(mysql_error());
+    $itemName = $subResult['itemName'];
+
+    echo "<li>".$itemName."</li>";
+}
+?>
