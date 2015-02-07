b4b0362198a34cee9f57fcf59219e511be0b1349
Ultron XIV <pmb14@ic.ac.uk>
2015-02-07 15:02:54 +0000

    php script to delete rows from database

---
+++ webapp/deleteListItem.php
@@ -0,0 +1,10 @@
+<?php
+
+$pID = $_GET["pID"];
+$itemID = $_GET["itemID"];
+
+$deleteRow = "DELETE FROM lists WHERE pID=$pID AND itemID=$itemID";
+
+mysql_query($deleteRow) or die(mysql_error());
+
+?>
