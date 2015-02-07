2e49c471b0e6bec2bc54026d2397dfd6a3af3bb2
Jan  Matas <jan.matas14@imperial.ac.uk>
2015-02-07 17:53:39 +0100

    voice added

--- raspi/main.py
+++ raspi/main.py
@@ -1,14 +1,34 @@
-import InputParser
+import inParser
 import db
-
-dbase = DB()
+import curses
+import getch
+import os
+import subprocess
+dbase = db.DB()
 dbase.connect()
 bindings = dbase.getBindings()
+names = dbase.getNames()
 dbase.close()
 
-instring = input()
+
+while 1:
+    char = getch.getch()
+    try:
+        parser = inParser.InputParser(char, bindings)
+        itemId = parser.getValue()
+        
+        dbase = db.DB()
+        dbase.connect()
+        
+        if not dbase.checkPresence(itemId): 
+            dbase.addToList(itemId)
+            name = names[itemId]
+            text = "%s added to the list." % name
+        else:
+            text = "item is already in the list"
+        dbase.close()       
+        subprocess.call('echo '+text+'|festival --tts', shell=True)
+    except:
+        print "Press correct buttonn you shithead"   
 
 
-
-parser = InputParser(instring, bindings)
-print parser.getValue()
