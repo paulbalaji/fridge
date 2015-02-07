a93451bafe6c558ccd317af393edcb338898cdf7
Ultron XIV <pmb14@ic.ac.uk>
2015-02-07 16:18:20 +0000

    added PDO to getList as well, pls check it's not messed up

--- webapp/getList.php
+++ webapp/getList.php
@@ -4,15 +4,23 @@
 #connect to MySQL
 include("includes/meta.php");
 
-$query = "SELECT itemID FROM lists WHERE pID=$pID";
+#$stmt = $dbh->prepare("SELECT itemID FROM lists WHERE pID=?");
+#$stmt->bindParam(1, $result, PDO::PARAM_STR, $safeID);
+#$safeID = $pID;
+#stmt->execute();
 
+$query = "SELECT itemID FROM lists WHERE pID=$pID";
 $result = mysql_query($query) or die(mysql_error());
 
 while($row = mysql_fetch_array($result)){
+    #$stmt2 = $dbh->prepare("SELECT * FROM items WHERE itemID=?");
+    #$stmt2->bindParam(1, $result2, PDO::PARAM_STR, $itemID);
     $itemID = $row['itemID'];
-    $subQuery = "SELECT * from items WHERE itemID=$itemID";
+    #stmt2->execute();
     
+    $subQuery = "SELECT * from items WHERE itemID=$itemID"; 
     $result2 = mysql_query($subQuery) or die(mysql_error());
+    
     $subResult = mysql_fetch_array($result2);
     
     $itemName = $subResult['itemName'];
