11c39e641364394cbdb054a06b435adf74173179
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 01:52:27 +0000

    15230

--- webapp/addEmail.php
+++ webapp/addEmail.php
@@ -2,7 +2,8 @@
 
 include("includes/meta.php");
 
-$query = "INSERT INTO lists (pID, itemID) VALUES ($pID, $i)";
-$emailAdd = mysql_query($query) or die(mysql_error());
+$query = "SELECT email FROM devices WHERE pID=1";
+$result = mysql_query($query) or die(mysql_error());
+$emailID = $result['email'];
 
 ?>
