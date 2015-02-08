4253f3de23696b24be87198096975fc415becc7e
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 03:53:54 +0000

    fksdgb

--- webapp/updateEmail.php
+++ webapp/updateEmail.php
@@ -1,8 +1,8 @@
 <?php
 include("includes/meta.php");
 
-$pID = 1;#$_GET["pID"];
-$email = "hardcoded@this.place"#$_GET["email"];
+$pID = $_GET["pID"];
+$email = $_GET["email"];
 $update = "UPDATE devices SET email=$email WHERE pID=$pID";
 
 mysql_query($update) or die(mysql_error());
