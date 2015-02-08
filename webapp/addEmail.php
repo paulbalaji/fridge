e100e28d2cc13c09dc438261cdc1bb65f73c6d14
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 02:01:00 +0000

    20001

--- webapp/addEmail.php
+++ webapp/addEmail.php
@@ -3,8 +3,8 @@
 include("includes/meta.php");
 
 $emailID = "paulmonish@gmail.com";
-$query = "SELECT email FROM devices WHERE pID=$pID";
-$result = mysql_query($query) or die(mysql_error());
-$emailID = $result['email'];
+#$query = "SELECT email FROM devices WHERE pID=$pID";
+#$result = mysql_query($query) or die(mysql_error());
+#$emailID = $result['email'];
 
 ?>
