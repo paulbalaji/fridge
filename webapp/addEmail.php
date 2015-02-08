0faa2fba8471fb7a33dd7c39eefb84dcd2507dfd
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 02:35:26 +0000

    kfsdbfkhsbd

--- webapp/addEmail.php
+++ webapp/addEmail.php
@@ -2,9 +2,10 @@
 
 include("includes/meta.php");
 
-$emailID = "paulmonish@gmail.com";
-#$query = "SELECT email FROM devices WHERE pID=$pID";
-#$result = mysql_query($query) or die(mysql_error());
-#$emailID = $result['email'];
+#$emailID = "paulmonish@gmail.com";
+$query = "SELECT email FROM devices WHERE pID=$pID";
+$result = mysql_query($query) or die(mysql_error());
+$row = mysql_fetch_array($result);
+$emailID = $row['email'];
 
 ?>
