70a8141b00904c143512834dc9e977c6a37e4ffb
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 02:20:31 +0000

    ekfbhsdb

--- webapp/settings.php
+++ webapp/settings.php
@@ -39,7 +39,7 @@
         <div data-role="main" class="ui-content">
             
             <table>
-            <p>Email list to: </p></td>     
+            <p>Email list to: </p>     
             <input type="text" id="email" value="<?php echo $emailID; ?>" placeholder="Email Address" /></td>
             <td><a href="#" id="addCustomButton" data-role="button" data-icon="plus" data-iconpos="notext"></a></td>
             </table>
