1807e6109d5a09790aecf21fa9ab689b221eac3e
Sam Wood <sam@sam.uk.net>
2015-02-08 01:16:51 +0000

    ...

--- raspi/main.py
+++ raspi/main.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import pygame
 
-pygame.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
+pygame.mixer.init() # setup mixer to avoid sound lag
 pygame.init() 
 dbase = db.DB()
 dbase.connect()
