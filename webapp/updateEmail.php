1fe3c57a7c976abb8486fac61b48bf6f4b8081bb
Ultron XIV <pmb14@ic.ac.uk>
2015-02-08 03:56:07 +0000

    ekfhb

--- webapp/updateEmail.php
+++ webapp/updateEmail.php
@@ -3,7 +3,7 @@ include("includes/meta.php");
 
 $pID = $_GET["pID"];
 $email = $_GET["email"];
-$update = "UPDATE devices SET email=$email WHERE pID=$pID";
+$update = "UPDATE devices SET email='$email' WHERE pID=$pID";
 
 mysql_query($update) or die(mysql_error());
 
